778d5ed269f27df9910b1d96c038b514b3853d6f
ZT Zhang <victorzzt@gmail.com>
2026-06-19 01:12:00 +0000

    Added some metadata for FDroid submission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.victorzzt.mcclock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.9"
+        versionCode = 2
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
